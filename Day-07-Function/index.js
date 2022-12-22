9c1437e1f873d28a060f8351ca5e3e0fa854e309
Trive\mumin.ozturk <mumin.ozturk@trive.com>
2022-12-22 22:43:02 +0300

    new day

--- Day-07-Function/index.js
+++ Day-07-Function/index.js
@@ -51,7 +51,91 @@ function reversefunc(A, B, C, D, E) {
      let reversef = [A, B, C, D, E];
      console.log(reversef.reverse());
 }
-function addItem() {}
+const addItem = (arr) => {
+     const newArr = [];
+     for (const element of arr) {
+          newArr.unshift(element);
+     }
+     return newArr;
+};
+const removeItem = () => {
+     const newArr = ["First", "Second", "Thirth", "Fourth"];
+     const removeitem = newArr.shift();
+     return removeitem;
+};
+const sumofnumber = (num1, num2, num3, num4) => {
+     const calculate = num1 + num2 + num3 + num4;
+     return calculate;
+};
+const sumOfOdds = (number1, number2, number3) => {
+     if ((number1, number2, number3 % 2 == 0)) {
+          const calculate = "Null";
+          return calculate;
+     } else {
+          const calculate = number1 + number2 + number3;
+          return calculate;
+     }
+};
+const sumOfOfEven = (number1, number2, number3) => {
+     if ((number1, number2, number3 % 2 == 0)) {
+          const calculate = number1 + number2 + number3;
+          return calculate;
+     } else {
+          const calculate = "Null";
+          return calculate;
+     }
+};
+const macAddress = () => {
+     let macArray = [];
+
+     for (let index = 0; index < 13; index++) {
+          const randomNumber = Math.random() * 100;
+
+          macArray.push(Math.floor(randomNumber));
+     }
+     return macArray; //
+};
+const UserIp = () => {
+     let Userrandom = [];
+
+     for (let index = 0; index < 10; index++) {
+          const randomuser = Math.random() * 10;
+          Userrandom.push(Math.floor(randomuser));
+     }
+     return Userrandom;
+};
+const arrayOfHexaColors = () => {
+     let HexaColor = ["#"];
+
+     for (let index = 0; index < 3; index++) {
+          const randomuser = Math.random() * 100;
+          HexaColor.push(Math.floor(randomuser));
+     }
+     return HexaColor;
+};
+const arrayOfRgbColors = () => {
+     let RgbColor = [];
+
+     for (let index = 0; index < 3; index++) {
+          const randomuser = Math.random() * 100;
+          RgbColor.push(Math.floor(randomuser));
+          if (randomuser <= 9) {
+               randomuser + 100;
+               return randomuser;
+          }
+     }
+     return RgbColor;
+};
+const isEmpty = () => {
+     const isHaveEmpty = [1, 2, 3, 45];
+     console.log(isHaveEmpty.indexOf(3));
+};
+const reverseCountries = () => {
+     const country = ["Turkey", "Finland", "Germany ", "Egypt"];
+     return country;
+};
+const copy = reverseCountries().reverse();
+
 console.log(generateFullName());
 console.log(generateFullName("Alperen", "Bayraktar")); // functionu manuel olarak parametrelerini değiştirdik
 addNumbers(10, 20); // addNumbers fonksiyonuna iki parametre belirledik bunları topladık
@@ -66,3 +150,14 @@ person(50 * 9, 80); // Ağırlık hesapladık ikinci parametre yer çekimi
 VKI(64, 1.7 * 2); //Vücut kitle indeksini hesapladık boyu 1.7 olarak kiloyu 75 olarak belirledim
 showDateTime(); // Function ile date objesini çektik
 reversefunc(1, 2, 3, 4, 5); // ters çeviren fonksiyon yazdık
+console.log(addItem("DENEME").reverse()); //Deneme yazısını Additem fonksiyonuna ekledik
+console.log(removeItem()); //Başında ki first yazısını kaldırdık
+console.log(sumofnumber(3, 4, 5, 6, 7)); // Toplama functionu yazdık
+console.log(sumOfOdds(1, 2, 3)); // Tek sayı ise çarp işlemi
+console.log(sumOfOfEven(6, 4, 2)); //Çift sayı ise çarp işlemi
+console.log(macAddress()); //Macc addresi oluşturduk rastgele
+console.log(UserIp()); // User Ip oluşturduk rastgele
+console.log(arrayOfHexaColors().join()); // Rastgele hexadecimal renk kodu oluşturduk
+console.log(arrayOfRgbColors()); //Rastgele rgb renk kodu oluşturduk
+console.log(isEmpty()); // isEmpty fonksiyonu ile bir sayıyı aradık
+console.log(copy); // Reversecountry fonksiyonunu kopyalayıp ters çevirdik
